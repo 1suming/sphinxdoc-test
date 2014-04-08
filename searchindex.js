a2c2f32ec529dbd3ebc8af405e4673e73b2285bf
author <1suming@sina.com>
2014-04-08 15:15:55 +0800

    rebuil docs

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index","includeme"],titles:["Welcome to scheme&#8217;s documentation!","&lt;no title&gt;"],objects:{},titleterms:{scheme:0,tabl:0,welcom:0,indic:0,document:0}})
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index","includeme"],titles:["Welcome to scheme&#8217;s documentation!","&lt;no title&gt;"],objects:{},titleterms:{document:0,welcom:0,sheme:0,indic:0,tabl:0,scheme:0}})
